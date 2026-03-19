715e7d54a53751f84831f90d63b1a70ae2ff9f74
anoor244 <farhanmoaiez244@gmail.com>
2026-03-19 16:40:50 +0530

    accurate email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -93,10 +93,10 @@ export function Footer() {
               <li className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail text-secondary shrink-0" aria-hidden="true"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"></path><rect x="2" y="4" width="20" height="16" rx="2"></rect></svg>
                 <Link
-                  href="mailto:trade@zavora-tradeaxis.com"
+                  href="mailto:info@zavoratradeaxis.com"
                   className="ml-2 hover:text-secondary"
                 >
-                  trade@zavora-tradeaxis.com
+                  info@zavoratradeaxis.com
                 </Link>
               </li>
               <li className="flex items-center">
