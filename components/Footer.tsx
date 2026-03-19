9e8af7ff8ec00e864bfad025b7d940de89a6c43d
anoor244 <farhanmoaiez244@gmail.com>
2026-03-19 16:58:29 +0530

    Extra email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -92,12 +92,17 @@ export function Footer() {
               </li>
               <li className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail text-secondary shrink-0" aria-hidden="true"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"></path><rect x="2" y="4" width="20" height="16" rx="2"></rect></svg>
-                <Link
-                  href="mailto:info@zavoratradeaxis.com"
-                  className="ml-2 hover:text-secondary"
-                >
-                  info@zavoratradeaxis.com
-                </Link>
+                <div className="flex gap-3 flex-wrap">
+    <a href="mailto:info@zavoratradeaxis.com" className="hover:text-secondary">
+      info@zavoratradeaxis.com
+    </a>
+
+    <span>|</span>
+
+    <a href="mailto:sales@zavoratradeaxis.com" className="hover:text-secondary">
+      sales@zavoratradeaxis.com
+    </a>
+  </div>
               </li>
               <li className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-phone text-secondary shrink-0" aria-hidden="true"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
